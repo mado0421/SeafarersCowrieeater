c528f7cc308a6c250b27d3ec7befbc657d19b893
신재욱 <sju421@gmail.com>
2025-01-26 21:11:57 +0900

    Create DiscordMessageSender.cs

---
+++ DiscordMessageSender.cs
@@ -0,0 +1,39 @@
+using Discord;
+using Discord.WebSocket;
+
+namespace SeafarersCowrieeater;
+
+public class DiscordMessageSender
+{
+    public async Task SendAsync(string token, string text, Stream imagesStream)
+    {
+        DiscordSocketClient client = new DiscordSocketClient();
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        var textChannels = GetTextChannels(client);
+
+        foreach (var textChannel in textChannels) 
+            await textChannel.SendFileAsync(imagesStream, text);
+    }
+
+    private IReadOnlyCollection<SocketTextChannel> GetTextChannels(DiscordSocketClient client)
+    {
+        List<SocketTextChannel> textChannels = [];
+        
+        foreach (var server in client.Guilds)
+        {
+            foreach (var textChannel in server.TextChannels)
+            {
+                var permissions = textChannel.GetPermissionOverwrite(client.CurrentUser);
+                
+                if (permissions is null) continue;
+                if (permissions.Value.SendMessages is not PermValue.Allow) continue;
+                
+                textChannels.Add(textChannel);
+            }
+        }
+
+        return textChannels;
+    }
+}
