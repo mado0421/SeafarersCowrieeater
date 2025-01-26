77865dc2d3ab5930e61887fa8895e5a631f4d303
신재욱 <sju421@gmail.com>
2025-01-26 21:59:33 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using SeafarersCowrieeater;
+
+TweetGetter tweetGetter = new TweetGetter();
+DiscordMessageSender discordMessageSender = new DiscordMessageSender();
+
+string twitterToken = "";
+string discordToken = "";
+string twitterId = "";
+List<string> keywords = [""];
+
+var tweetData = await tweetGetter.GetTweet(twitterToken, twitterId, keywords);
+await discordMessageSender.SendAsync(discordToken, tweetData.Text, tweetData.Image);
