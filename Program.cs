11aed544fa78b7a7c3aaae6899eafd1ff785ad87
신재욱 <sju421@gmail.com>
2025-01-27 02:41:27 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SeafarersCowrieeater;
 
-using SeafarersCowrieeater;
+var tweetGetter = new TweetGetter();
+var discordMessageSender = new DiscordMessageSender();
+List<string> keywords = new List<string> { "#무인도의고양이", "Week" };
 
-TweetGetter tweetGetter = new TweetGetter();
-DiscordMessageSender discordMessageSender = new DiscordMessageSender();
+// 환경 변수 값 읽기
+string? twitterToken = Environment.GetEnvironmentVariable("TWITTER_TOKEN");
+string? discordToken = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+string? twitterId = Environment.GetEnvironmentVariable("TWITTER_ID");
 
-string twitterToken = "";
-string discordToken = "";
-string twitterId = "";
-List<string> keywords = [""];
-
-var tweetData = await tweetGetter.GetTweet(twitterToken, twitterId, keywords);
-await discordMessageSender.SendAsync(discordToken, tweetData.Text, tweetData.Image);
+// null 확인 및 기본값 처리
+if (string.IsNullOrEmpty(twitterToken) || string.IsNullOrEmpty(discordToken) || string.IsNullOrEmpty(twitterId))
+{
+    Console.WriteLine("필수 환경 변수가 설정되지 않았습니다.");
+    return;
+}
+            
+var tweet = await tweetGetter.GetTweet(twitterToken, twitterId, keywords);
+await discordMessageSender.SendAsync(discordToken, twitterId, tweet.Id, tweet.MediaUrls);
